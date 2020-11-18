74975e64d1ca3855faecd1952f7aca928fbd4c9e
Erik Oosterwaal <erik.oosterwaal@cm.com>
2020-11-18 16:25:49 +0100

    Added manifest.

---
+++ Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "CMPush",
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "CMPush",
+            targets: ["CMPushPackage"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+    ],
+    targets:[
+        .binaryTarget(name: "CMPushPackage", url: "https://github.com/cmdotcom/text-push-library-ios/blob/main/CMPush.xcframework.zip", checksum: "676a4b6d1d51cd10979d1902919f82fa3d4a04b9c2ce5871bdc85d1e2ddfd93e")
+        ])
---
+++ README.md
@@ -0,0 +1,7 @@
+# CMPush
+
+Library for integrating CM push into your app.
+
+Please see https://www.cm.com/app/docs/en/api/business-messaging-api/1.0/index#push for details.
+
+
