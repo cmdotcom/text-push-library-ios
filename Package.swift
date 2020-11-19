8564579dbab6d34435d4738a4ad2c0f888bc0b19
Erik Oosterwaal <erik.oosterwaal@cm.com>
2020-11-19 14:43:27 +0100

    Link to the raw zip file, it was directing to a github page.

--- Package.swift
+++ Package.swift
@@ -16,5 +16,5 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets:[
-        .binaryTarget(name: "CMPush", url: "https://github.com/cmdotcom/text-push-library-ios/blob/main/CMPush.xcframework.zip", checksum: "5b09862e77be7e1b802679eed01136f4d1919a47563ce012c226f488a6f411ea")
+        .binaryTarget(name: "CMPush", url: "https://github.com/cmdotcom/text-push-library-ios/raw/main/CMPush.xcframework.zip", checksum: "5b09862e77be7e1b802679eed01136f4d1919a47563ce012c226f488a6f411ea")
         ])
