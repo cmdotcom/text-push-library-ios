f692a91f0a22f9e77804563b423fccc61296111f
Erik Oosterwaal <erik.oosterwaal@cm.com>
2020-11-19 16:16:53 +0100

    Checksum change

--- Package.swift
+++ Package.swift
@@ -16,5 +16,5 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets:[
-		.binaryTarget(name: "CMPush", url: "https://www.cm.com/cdn/packages/push/CMPush.xcframework.zip", checksum: "5b09862e77be7e1b802679eed01136f4d1919a47563ce012c226f488a6f411ea")
+		.binaryTarget(name: "CMPush", url: "https://www.cm.com/cdn/packages/push/CMPush.xcframework.zip", checksum: "676a4b6d1d51cd10979d1902919f82fa3d4a04b9c2ce5871bdc85d1e2ddfd93e")
 	])
