3e478c57d8f3e26b085b7dd321b319bcc9b00233
Erik Oosterwaal <erik.oosterwaal@cm.com>
2020-11-19 14:58:25 +0100

    Using path instead of URL

---
+++ CMPush.xcframework/ios-arm64/CMPush.framework/Headers/CMPush.h
@@ -0,0 +1,18 @@
+//
+//  CMPush.h
+//  CMPush
+//
+//  Created by Bert Smalen on 26/10/2020.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CMPush.
+FOUNDATION_EXPORT double CMPushVersionNumber;
+
+//! Project version string for CMPush.
+FOUNDATION_EXPORT const unsigned char CMPushVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CMPush/PublicHeader.h>
+
+
